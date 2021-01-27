a3ed6be6fb2e91d2c459c468d1cb38e915b81901
Christian <cmendieta@lumbrera.com.mx>
2021-01-27 16:38:00 -0600

    add animations page about

--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -1,104 +1,126 @@
 <template>
   <div class="about-view">
-    <section class="relative bg-about">
+    <section ref="intro" class="relative bg-about">
       <div class="overlay-section"></div>
       <b-container class="h40 d-flex align-items-center">
         <b-row class="mt-5 text-white">
           <b-col>
-            <h1>Quienes somos...</h1>
+            <h1 class="h2">Quienes somos...</h1>
           </b-col>
         </b-row>
       </b-container>
     </section>
-    <section class="bg-white h100 py-5">
+    <section ref="mision" class="bg-white h100 py-5">
       <b-container>
         <b-row class="justify-content-center">
-          <b-col sm="10" md="5">
-            <h2 class="mt-5 d-flex align-items-center">
-              <Pulse />
-              <span class="text-danger">M</span>isión
-            </h2>
-            <p>
-              Transcopar es único e inigualable; considerando la gama enorme que
-              tenemos como competencia en el servicio del transporte, transcopar
-              y sus colaboradores día con día presentan una experiencia nueva la
-              cual nos acompaña a nuestros destinos contar con cada uno de
-              nuestros clientes es lo fundamental ya que gracias a ellos podemos
-              seguir formando más experiencia y dedicación. Innovando programas
-              administrativos, de organización flexible , enfocada al cliente y
-              de servicio que fomente el crecimiento de nuestra empresa.
-            </p>
-          </b-col>
+          <transition name="fade-in" appear>
+            <b-col v-if="showIntro" sm="10" md="5">
+              <h2 class="mt-5 d-flex align-items-center">
+                <Pulse />
+                <span class="text-danger">M</span>isión
+              </h2>
+              <p>
+                Transcopar es único e inigualable; considerando la gama enorme
+                que tenemos como competencia en el servicio del transporte,
+                transcopar y sus colaboradores día con día presentan una
+                experiencia nueva la cual nos acompaña a nuestros destinos
+                contar con cada uno de nuestros clientes es lo fundamental ya
+                que gracias a ellos podemos seguir formando más experiencia y
+                dedicación. Innovando programas administrativos, de organización
+                flexible , enfocada al cliente y de servicio que fomente el
+                crecimiento de nuestra empresa.
+              </p>
+            </b-col>
+          </transition>
           <b-col sm="10" offset-md="1" md="6">
-            <img
-              class="right-img shadow"
-              src="../../assets/img/index3.jpg"
-              alt="Transcopar"
-            />
+            <transition name="slide-fade">
+              <div v-if="showImageOne" class="w-100">
+                <img
+                  class="right-img shadow"
+                  src="../../assets/img/index3.jpg"
+                  alt="Transcopar"
+                />
+              </div>
+            </transition>
           </b-col>
         </b-row>
         <b-row class="justify-content-center d-flex align-items-end">
           <b-col sm="10" md="5">
-            <h2 class="mt-5 d-flex align-items-center">
-              <Pulse />
-              <span class="text-danger">V</span>isión
-            </h2>
-            <p>
-              Ser la empresa líder a nivel nacional en el sector transporte
-              brindando un servicio de calidad, manteniéndonos dentro del
-              mercado en el margen de rentabilidad de la empresa solicitado.
-              Siempre teniendo en primer término el cliente y su satisfacción.
-            </p>
-            <img
-              class="left-img shadow"
-              src="../../assets/img/index2.jpg"
-              alt="Transcopar"
-            />
+            <transition name="fade-in">
+              <div v-if="showVision" class="w-100">
+                <h2 class="mt-5 d-flex align-items-center">
+                  <Pulse />
+                  <span class="text-danger">V</span>isión
+                </h2>
+                <p>
+                  Ser la empresa líder a nivel nacional en el sector transporte
+                  brindando un servicio de calidad, manteniéndonos dentro del
+                  mercado en el margen de rentabilidad de la empresa solicitado.
+                  Siempre teniendo en primer término el cliente y su
+                  satisfacción.
+                </p>
+              </div>
+            </transition>
+            <transition name="slide-fade-left">
+              <img
+                v-if="showImageTwo"
+                class="left-img shadow"
+                src="../../assets/img/index2.jpg"
+                alt="Transcopar"
+              />
+            </transition>
           </b-col>
           <b-col sm="10" offset-md="1" md="6" class="my-3 my-sm-5">
-            <h2 class="mt-5 d-flex align-items-center">
-              <Pulse />
-              <span class="text-danger">T</span>ranscopar
-            </h2>
-            <p>
-              Consideramos indispensable contar con un sistema operativo que
-              garantice la satisfacción de nuestro cliente innovando y renovando
-              cada segmento que forma la empresa. Realizar fletes es nuestra
-              labor más grande pero culminarla es nuestro éxito porque servirte
-              queda reflejado en tu satisfacción total. Se cuenta con rastreo
-              satelital en cada una de las unidades que contamos, las 24hrs del
-              día y los 365 días del año, complementado con teléfono móvil para
-              mantenernos comunicados a nuestros clientes y ofrecer mejor
-              servicio.
-            </p>
+            <transition name="fade-in">
+              <div v-if="showTranscopar" class="w-100">
+                <h2 class="mt-5 d-flex align-items-center">
+                  <Pulse />
+                  <span class="text-danger">T</span>ranscopar
+                </h2>
+                <p>
+                  Consideramos indispensable contar con un sistema operativo que
+                  garantice la satisfacción de nuestro cliente innovando y
+                  renovando cada segmento que forma la empresa. Realizar fletes
+                  es nuestra labor más grande pero culminarla es nuestro éxito
+                  porque servirte queda reflejado en tu satisfacción total. Se
+                  cuenta con rastreo satelital en cada una de las unidades que
+                  contamos, las 24hrs del día y los 365 días del año,
+                  complementado con teléfono móvil para mantenernos comunicados
+                  a nuestros clientes y ofrecer mejor servicio.
+                </p>
+              </div>
+            </transition>
           </b-col>
         </b-row>
       </b-container>
     </section>
-    <section class="py-5 bg-gray">
+    <section class="py-5 bg-gray h80 d-flex align-items-center">
       <b-container>
         <b-row class="mt-5">
           <b-col>
-            <div class="box-banner shadow mb-5">
-              <b-row class="justify-content-center">
-                <b-col sm="8" md="6" class="box-img shadow">
-                  <img src="../../assets/img/logo.png" alt="transcopar" />
-                </b-col>
-                <b-col sm="10">
-                  <h3 class="font-weight-normal">
-                    "Generación tras generación manteniendo los mismos valores
-                    que son los que los caracterizan siempre teniendo como
-                    primer término el mejor servicio para nuestros clientes..."
-                  </h3>
-                  <h4 class="h6 mt-4">
-                    Giovanni Paredes
-                    <span class="font-weight-normal"
-                      >| Director General de Transcopar S.A. de C.V.</span
-                    >
-                  </h4>
-                </b-col>
-              </b-row>
-            </div>
+            <transition name="fade-in">
+              <div v-if="showBanner" class="box-banner shadow mb-5">
+                <b-row class="justify-content-center">
+                  <b-col sm="8" md="6" class="box-img shadow">
+                    <img src="../../assets/img/logo.png" alt="transcopar" />
+                  </b-col>
+                  <b-col sm="10">
+                    <h3 class="font-weight-normal">
+                      "Generación tras generación manteniendo los mismos valores
+                      que son los que los caracterizan siempre teniendo como
+                      primer término el mejor servicio para nuestros
+                      clientes..."
+                    </h3>
+                    <h4 class="h6 mt-4">
+                      Giovanni Paredes
+                      <span class="font-weight-normal"
+                        >| Director General de Transcopar S.A. de C.V.</span
+                      >
+                    </h4>
+                  </b-col>
+                </b-row>
+              </div>
+            </transition>
           </b-col>
         </b-row>
       </b-container>
@@ -106,7 +128,54 @@
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      showIntro: false,
+      showImageOne: false,
+      showVision: false,
+      showImageTwo: false,
+      showTranscopar: false,
+      showBanner: false,
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  created() {
+    setTimeout(() => {
+      this.showIntro = true
+      setTimeout(() => {
+        this.showImageOne = true
+      }, 300)
+    }, 400)
+  },
+  methods: {
+    handleScroll() {
+      const intro = this.$refs.intro.clientHeight
+      const mision = this.$refs.mision.clientHeight
+
+      if (window.scrollY > intro + 200) {
+        this.showVision = true
+      }
+
+      if (window.scrollY > intro + 400) {
+        this.showImageTwo = true
+      }
+
+      if (window.scrollY > intro + 450) {
+        this.showTranscopar = true
+      }
+
+      if (window.scrollY > intro + (mision / 5) * 4) {
+        this.showBanner = true
+      }
+    },
+  },
+}
 </script>
 <style scoped>
 .about-view {
